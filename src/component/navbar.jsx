6d24e3026905ff08c62a962a2f8e4d78a7876779
jacktampan <mochamadzakaria7@gmail.com>
2024-06-03 15:35:10 +0700

    Update navbar.jsx

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,24 +1,30 @@
 import React, { useState } from "react";
 import Logoimg from "../assets/logo.png";
+
 const NavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const toggleDialog = () => {
+    setIsDialogOpen(!isDialogOpen);
+  };
+
   const links = [
-    { name: "Home", href: "#" },
+    { name: "Home", href: "/" },
     { name: "Services", href: "/list" },
     { name: "Contact", href: "#" },
-    { name: "Masuk/Daftar", href: "/register", isButton: true },
+    { name: "Masuk/Daftar", href: "#", isButton: true, onClick: toggleDialog },
   ];
 
   return (
     <nav className="bg-white dark:bg-zinc-800 container mx-auto">
       <div className="flex justify-around">
         <div className="flex items-center py-5 px-2">
-          <a href="#">
+          <a href="/">
             <img src={Logoimg} alt="Logo" className="h-36" />
           </a>
         </div>
@@ -32,12 +38,12 @@ const NavBar = () => {
               {link.name}
             </a>
           ))}
-          <a
-            href={links[3].href}
+          <button
+            onClick={toggleDialog}
             className="py-2 px-3 bg-blue-500 text-white rounded hover:bg-blue-600"
           >
             Masuk/Daftar
-          </a>
+          </button>
         </div>
         <div className="md:hidden flex items-center">
           <button
@@ -66,7 +72,9 @@ const NavBar = () => {
       </div>
       <div
         id="mobile-menu"
-        className={`mobile-menu md:hidden ${isMenuOpen ? "" : "hidden"}`}
+        className={`mobile-menu md:hidden ${
+          isMenuOpen ? "flex" : "hidden"
+        } flex-col items-center`}
       >
         {links.slice(0, 3).map((link) => (
           <a
@@ -77,13 +85,62 @@ const NavBar = () => {
             {link.name}
           </a>
         ))}
-        <a
-          href={links[3].href}
+        <button
+          onClick={toggleDialog}
           className="block py-2 px-4 text-sm bg-blue-500 text-white rounded hover:bg-blue-600"
         >
           Masuk/Daftar
-        </a>
+        </button>
       </div>
+
+      {isDialogOpen && (
+        <div
+          className="relative z-10"
+          aria-labelledby="modal-title"
+          role="dialog"
+          aria-modal="true"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+              <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <div className="justify-center sm:flex sm:items-start">
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                      <h3
+                        className="text-base font-semibold leading-6 text-gray-900"
+                        id="modal-title"
+                      >
+                        Masuk ke CozyKost
+                      </h3>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          Saya ingin masuk sebagai
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 justify-center sm:flex sm:flex-row-reverse sm:px-6">
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:ml-3 sm:w-auto"
+                  >
+                    Pencari Kost
+                  </button>
+                  <button
+                    type="button"
+                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    onClick={toggleDialog}
+                  >
+                    Pemilik Kost
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
