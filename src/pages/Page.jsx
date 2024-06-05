35d9757eeed50a6c333fb74b43f13085271ea2df
jacktampan <mochamadzakaria7@gmail.com>
2024-06-05 09:54:35 +0700

    update

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -1,12 +1,16 @@
 import NavBar from "../component/navbar";
 import Page from "../component/page";
 import Footer from "../component/footer";
+import Review from "../component/review";
 
 export default function PagePage() {
   return (
     <>
       <NavBar />
       <Page />
+      <div className="container mx-auto p-14">
+        <Review />
+      </div>
       <Footer />
     </>
   );
