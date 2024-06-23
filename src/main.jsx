83b2857af118dd3cd7e00969f710d664719ee0a8
jacktampan <mochamadzakaria7@gmail.com>
2024-06-23 23:43:15 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./App.css";
 
+const removeDarkMode = () => {
+  const root = document.documentElement; // Biasanya <html>
+  root.classList.remove("dark");
+};
+
+function Main() {
+  useEffect(() => {
+    removeDarkMode();
+  }, []);
+
+  return <App />;
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="font-sans">
